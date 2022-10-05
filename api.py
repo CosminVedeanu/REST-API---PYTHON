4d8ddc7dd8ebaa2bd69d016108d28e8c0bddc77e
CosminVedeanu <112734408+CosminVedeanu@users.noreply.github.com>
2022-10-05 15:31:18 +0300

    V.1.3

--- api.py
+++ api.py
@@ -1,59 +1,125 @@
 import requests
-from auth import api_key, api_url
+from auth import api_key, base_url
 
-#GET
-class API:
+class Api:
+    junk = []
     def __init__(self, id):
         self.api_key = api_key
-        self.api_url = f'{api_url}users'
         self.id = id
+        self.response = {}
     # GET
     def fetch(self):
-        response = requests.get(self.api_url, headers=self.api_key)
+        response = requests.get(self.API_URL, headers=self.api_key)
         print('GET', response.json(), response.status_code, sep='\n')
+        self.response = response
         return response
 
     # POST
     def create(self):
-        response = requests.post(self.api_url, json=self.payload, headers=self.api_key)
+        response = requests.post(self.API_URL, json=self.payload, headers=self.api_key)
         print('POST', response.json(), response.status_code, sep='\n')
-        try:
-            self.id = response.json()['id']
-            return self.id
-        except TypeError:
-            print("Id already in use")
+        self.id = response.json()['id']
+        self.api_url_id = f'{self.API_URL}/{self.id}'
+        self.response = response
+        self.junk.append(self.api_url_id)
+        return self.id
+
     # PATCH
     def update(self):
         response = requests.patch(self.api_url_id, json=self.payload, headers=self.api_key)
         print('PATCH', response.json(), response.status_code, sep='\n')
+        self.response = response
 
     # PUT
     def upgrade(self):
         response = requests.put(self.api_url_id, json=self.payload, headers=self.api_key)
         print('PUT', response.json(), response.status_code, sep='\n')
+        self.response = response
 
     # DELETE
     def delete(self):
         response = requests.delete(self.api_url_id, headers=self.api_key)
         print('DELETE', response.status_code, sep='\n')
+        self.response = response
+
+    def cleanup(self):
+        for i in range(0, len(self.junk)):
+            response = requests.delete(self.junk[i], headers=self.api_key)
+            print(f'ID:{self.junk[i]} removed!', response.status_code, sep='\n')
 
     # PAYLOAD
-class Users(API):
-    def set_payload(self, name, email, gender, status):
-        self.payload = {"name": name, "email": email, "gender": gender, "status": status}
-        self.api_url_id = f'{self.api_url}/{self.id}'
+class Users(Api):
+    API_URL = f'{base_url}users'
 
-class Posts(API):
-    def set_payload(self, user_id, title, body):
-        self.payload = {"user_id": user_id, "title": title, "body": body}
-        self.api_url_id = f'{self.api_url}/{self.id}'
+    def __init__(self, name, email, gender, status, id=''):
 
-class Comments(API):
-    def set_payload(self, post_id, name, email, body):
-        self.payload = {"post_id": post_id, "name": name, "email": email, "body": body}
-        self.api_url_id = f'{self.api_url}/{self.id}'
+        self.name = name
+        self.email = email
+        self.gender = gender
+        self.status = status
+        super().__init__(id)
 
-class Todos(API):
-    def set_payload(self, user_id, title, due_on, status):
-        self.payload = {"user_id": user_id, "title": title, "due_on": due_on, "status": status}
-        self.api_url_id = f'{self.api_url}/{self.id}'
+    def set_payload(self):
+
+        self.payload = {
+            "name": self.name,
+            "email": self.email,
+            "gender": self.gender,
+            "status": self.status
+        }
+
+class Posts(Api):
+    API_URL = f'{base_url}posts'
+
+    def __init__(self, user_id, title, body, id=''):
+        self.user_id = user_id
+        self.title = title
+        self.body = body
+        super().__init__(id)
+
+    def set_payload(self):
+
+        self.payload = {
+            "user_id": self.user_id,
+            "title": self.title,
+            "body": self.body
+        }
+
+class Comments(Api):
+    API_URL = f'{base_url}comments'
+
+    def __init__(self, post_id, name, email, body, id=''):
+        self.post_id = post_id
+        self.name = name
+        self.email = email
+        self.body = body
+        super().__init__(id)
+
+    def set_payload(self):
+
+        self.payload = {
+            "post_id": self.post_id,
+            "name": self.name,
+            "email": self.email,
+            "body": self.body
+        }
+
+class Todos(Api):
+    API_URL = f'{base_url}todos'
+
+    def __init__(self, user_id, title, due_on, status, id=''):
+        self.user_id = user_id
+        self.title = title
+        self.due_on = due_on
+        self.status = status
+        super().__init__(id)
+
+
+    def set_payload(self):
+
+        self.payload = {
+            "user_id": self.user_id,
+            "title": self.title,
+            "due_on": self.due_on,
+            "status": self.status
+        }
--- main.py
+++ main.py
@@ -1,57 +1,59 @@
-from API import Users, Posts, Comments, Todos
+from api import Api, Users, Posts, Comments, Todos
+import random
 
 #users
-user_id = '6180'
-name = 'Cosmin21'
-email = 'Cosmin@45215.com'
+name = f'Cosmin{random.random()}'
+email = f'Cosmin@{random.random()}.com'
 gender = 'male'
 user_status = 'active'
 
 #posts
-post_title = 'Carbunele1'
-post_body = 'Carbunele este o roca sedimentara1'
+post_title = f'Carbunele{random.random()}'
+post_body = f'Carbunele este o roca sedimentara.{random.random()}'
 
 #comments
-comment_body = 'Foarte informativ1'
+comment_body = f'Foarte informativ.{random.random()}'
 
 #todos
-todo_title = 'Carbunele1'
+todo_title = f'Carbunele{random.random()}'
 due_on = '2022-10-18T00:00:00.000+05:30'
 todo_status = 'pending'
 
-
-users = Users(user_id)
+users = Users(name, email, gender, user_status)
 get = users.fetch() #200
-users.set_payload(name, email, gender, user_status)
+users.set_payload()
 user_id = users.create() #201
-users.set_payload(name, email, gender, user_status)
+users.set_payload()
 users.upgrade() #200
 users.update() #200
 #users.delete() #204
 
-posts = Posts(user_id)
+posts = Posts(user_id, post_title, post_body)
 get = posts.fetch() #200
-posts.set_payload(user_id, post_title, post_body)
+posts.set_payload()
 post_id = posts.create() #201
-posts.set_payload(user_id, post_title, post_body)
+posts.set_payload()
 posts.upgrade() #200
 posts.update() #200
 #posts.delete() #204
 
-comments = Comments(post_id)
+comments = Comments(post_id, user_id, email, comment_body)
 get = comments.fetch() #200
-comments.set_payload(post_id, user_id, email, comment_body)
+comments.set_payload()
 comment_id = comments.create() #201
-comments.set_payload(post_id, user_id, email, comment_body)
+comments.set_payload()
 comments.upgrade() #200
 comments.update() #200
 #comments.delete() #204
 
-todos = Todos(user_id)
+todos = Todos(user_id, todo_title, due_on, todo_status)
 get = todos.fetch() #200
-todos.set_payload(user_id, todo_title, due_on, todo_status)
+todos.set_payload()
 todo_id = todos.create() #201
-todos.set_payload(user_id, todo_title, due_on, todo_status)
+todos.set_payload()
 todos.upgrade() #200
 todos.update() #200
 #todos.delete() #204
+
+clean = Api(1)
+clean.cleanup()
